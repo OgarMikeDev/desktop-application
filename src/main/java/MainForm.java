1be21a2e4b5ae9bc830fb02b172a4493402e27b3
Mishail Ogar <muraviov.m.v@mail.ru>
2023-01-13 14:24:57 +0300

    "HomeWorkDesktopApplication"

--- src/main/java/MainForm.java
+++ src/main/java/MainForm.java
@@ -2,16 +2,16 @@ import javax.swing.*;
 
 public class MainForm {
     private JPanel mainPanel;
-    private JTextField Name;
-    private JTextField Surname;
-    private JTextField SurnameTwo;
-    private JButton CollapsButton;
-
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-    }
+    private JButton button1;
+    private JTextArea nameTextArea;
+    private JTextArea surnameTwoTextArea;
+    private JTextArea surnameOneTextArea;
 
     public JPanel getMainPanel() {
         return mainPanel;
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
