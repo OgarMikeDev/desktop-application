dd460874162880262d5e5011f4e791754ec3e096
Mishail Ogar <muraviov.m.v@mail.ru>
2023-01-13 12:42:16 +0300

    "HomeWorkDesktopApplication"

---
+++ src/main/java/MainForm.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class MainForm {
+    private JPanel mainPanel;
+    private JTextField Name;
+    private JTextField Surname;
+    private JTextField SurnameTwo;
+    private JButton CollapsButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+}
